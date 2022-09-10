639aaa5c821706fe45ac8938ff6675beb59419e5
zuby213 <onwuasoanyanzube@gmail.com>
2022-09-10 05:54:13 -0700

    task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- prints lower case alphabets
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char letters;
+
+for (letters = 'a'; letters <= 'z'; letters++)
+	putchar(letters);
+putchar('\n');
+return (0);
+}
