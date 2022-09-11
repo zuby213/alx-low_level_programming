ce6b1cae7b611d69290f89dc866447760f73c62d
zuby213 <onwuasoanyanzube@gmail.com>
2022-09-11 06:30:36 -0700

    task 11 commit corrections

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,7 +20,7 @@ int main(void)
 					putchar(d);
 					putchar(p);
 					putchar(q);
-																					if (d == '7' && p == '8')
+					if (d == '7' && p == '8')
 						continue;
 					putchar(',');
 					putchar(' ');
