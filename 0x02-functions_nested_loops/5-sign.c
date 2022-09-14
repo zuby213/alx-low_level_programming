d1be6b0443578ad16476c98d334e9c32bad3f933
zuby213 <onwuasoanyanzube@gmail.com>
2022-09-14 05:13:40 -0700

    task 5 commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *  print_sign - Print sign
+ *
+ *  @n: Process input
+ *
+ *  Return: Always 1 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
