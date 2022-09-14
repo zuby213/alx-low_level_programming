0b510346c8b4a62b4af0218194e6ab3562bee6aa
zuby213 <onwuasoanyanzube@gmail.com>
2022-09-14 05:37:20 -0700

    task 6 commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _abs - Get absolute value
+ *
+ *  @n: Take argument
+ *
+ *  Return: Always n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
