c8dbfa9e971aeffb039c0e3156fb7b5f97b38188
zuby213 <onwuasoanyanzube@gmail.com>
2022-09-14 18:50:43 -0700

    task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - a function to sum
+ * of two integers.
+ *
+ * @a: input integer a
+ * @b: input integer b
+ * Return:Always int (success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
