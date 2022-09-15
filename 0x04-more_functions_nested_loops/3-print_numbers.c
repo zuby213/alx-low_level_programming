41df68385ea41914f4e93e067b61451d847200cf
zuby213 <onwuasoanyanzube@gmail.com>
2022-09-15 15:55:25 -0700

    task 3 commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers- function to print numbers
+ * from 0-9
+ *
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
