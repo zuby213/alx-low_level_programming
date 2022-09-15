c8dce9086fde874407b1498e2e51723c73454d04
zuby213 <onwuasoanyanzube@gmail.com>
2022-09-15 15:41:45 -0700

    task 2 commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - function that multiplies
+ *  two inputs
+ *
+ * @a:input a
+ * @b:input b
+ * Return: Always result of the mult.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
