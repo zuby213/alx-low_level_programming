69fb79718d349f6f038fdf1366496420b2916aff
zuby213 <onwuasoanyanzube@gmail.com>
2022-09-15 18:13:00 -0700

    task 9 commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *  print_square - print what
+ *
+ *  @size: see value
+ *
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+{
+	_putchar('\n');
+}
+	else
+{
+	for (i = 0; i < size; i++)
+{
+	for (j = 0; j < size; j++)
+{
+	_putchar(35);
+}
+	_putchar('\n');
+}
+}
+}
