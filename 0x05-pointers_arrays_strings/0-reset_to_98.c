d0eb030f8e7ab319280ed24e1acf759dbebdf3f6
zuby213 <onwuasoanyanzube@gmail.com>
2022-09-19 19:00:39 -0700

    task 0 commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that uses pointer to
+ *  reset a value in another function.
+ *
+ * @n: dereference
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
