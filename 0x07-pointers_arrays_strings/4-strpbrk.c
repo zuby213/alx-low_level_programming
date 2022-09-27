5b10a7c8bac0aecd5031cbeb1fabf5f84e83192f
zuby213 <onwuasoanyanzube@gmail.com>
2022-09-26 19:20:33 -0700

    task 4 commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *  _strpbrk - searches a string
+ *
+ *  @s: string
+ *
+ *  @accept: string bytes
+ *
+ *  Return: return pointer to byte in s or null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return ('\0');
+}
