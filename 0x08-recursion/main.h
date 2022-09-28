b1f00ced06fab3d3fb936979c47bc6553b18e627
zuby213 <onwuasoanyanzube@gmail.com>
2022-09-28 10:44:29 -0700

    task 0 commit

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+void _puts_recursion(char *s);
+void _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+#endif /*MAIN_H*/
